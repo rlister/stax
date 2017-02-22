0f33ca4e89a865ce0346eaabfcd646611d92e332
Richard Lister <rlister@gmail.com>
2017-02-22 15:08:28 -0500

    move staxfile commands to module methods

--- lib/stax/base.rb
+++ lib/stax/base.rb
@@ -1,28 +1,5 @@
 module Stax
   class Base < Thor
-    @@_stack_prefix = `git symbolic-ref --short HEAD`.chomp + '-'
-
-    def self.load_staxfile
-      file = File.join(Dir.pwd, 'Staxfile')
-      Stax::Base.class_eval(File.binread(file)) if File.exist?(file)
-    end
-
-    ## add a Stack subclass as a thor subcommand
-    def self.add_stack(name)
-      c = name.capitalize
-
-      ## create the class if it does not exist yet
-      klass = self.const_defined?(c) ? self.const_get(c) : self.const_set(c, Class.new(Stack))
-
-      ## create thor subcommand
-      Cli.desc(name, "control #{name} stack")
-      Cli.subcommand(name, klass)
-    end
-
-    def self.set_prefix(prefix)
-      @@_stack_prefix = prefix
-    end
-
     no_commands do
       def debug(message)
         say "[DEBUG] #{message}", :blue
