1965b1bdffcfb25229926d85c3b0c91b1fb0a753
Richard Lister <rlister@gmail.com>
2017-02-21 23:02:46 -0500

    move add_stack into Base and add Staxfile loading

--- lib/stax/base.rb
+++ lib/stax/base.rb
@@ -1,7 +1,25 @@
 require 'thor'
 
 module Stax
+
   class Base < Thor
+    def self.load_staxfile
+      file = File.join(Dir.pwd, 'Staxfile')
+      Stax::Base.class_eval(File.binread(file)) if File.exist?(file)
+    end
+
+    ## add a Stack subclass as a thor subcommand
+    def self.add_stack(name)
+      c = name.capitalize
+
+      ## create the class if it does not exist yet
+      klass = self.const_defined?(c) ? self.const_get(c) : self.const_set(c, Class.new(Stack))
+
+      ## create thor subcommand
+      Cli.desc(name, "control #{name} stack")
+      Cli.subcommand(name, klass)
+    end
+
     no_commands do
       def debug(message)
         say "[DEBUG] #{message}", :blue
--- lib/stax/stack.rb
+++ lib/stax/stack.rb
@@ -1,16 +1,4 @@
 module Stax
-  ## add a Stack subclass as a thor subcommand
-  def self.add_stack(name)
-    c = name.capitalize
-
-    ## create the class if it does not exist yet
-    klass = self.const_defined?(c) ? self.const_get(c) : self.const_set(c, Class.new(Stack))
-
-    ## create thor subcommand
-    Cli.desc(name, "control #{name} stack")
-    Cli.subcommand(name, klass)
-  end
-
   class Stack < Base
     include Awful::Short
 
