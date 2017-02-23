efcccf806e72af0ddbf9b20b6250a3298ff3e1a5
Richard Lister <rlister@gmail.com>
2017-02-22 22:40:35 -0500

    add elb mixin

---
+++ lib/stax/elb.rb
@@ -0,0 +1,33 @@
+require 'awful/elb'
+
+module Stax
+  module Elb
+    def self.included(thor)
+      thor.class_eval do
+
+        no_commands do
+          def load_balancers
+            @_load_balancers ||= cf(:resources, [stack_name], type: ['AWS::ElasticLoadBalancing::LoadBalancer'], quiet: true)
+          end
+
+          def elb_status
+            load_balancers.each do |elb|
+              debug("ELB status for #{elb.logical_resource_id}")
+              elb(:instances, [elb.physical_resource_id], long: true)
+            end
+          end
+        end
+
+        desc 'elb', 'show DNS name for stack ELBs'
+        def elbs
+          debug("Getting ELBs for stack #{stack_name}")
+          load_balancers.each do |elb|
+            debug("DNS name for #{elb.logical_resource_id}")
+            elb(:dns, [elb.physical_resource_id])
+          end
+        end
+
+      end
+    end
+  end
+end
