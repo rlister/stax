bb04cc78eeda88d5057854a2bc2d9026a0d10035
Richard Lister <rlister@gmail.com>
2017-03-06 22:48:42 -0500

    improve alb tasks, add cmdline task for albs

--- lib/stax/alb.rb
+++ lib/stax/alb.rb
@@ -6,22 +6,28 @@ module Stax
       thor.class_eval do
 
         no_commands do
-          def app_load_balancers
-            @_app_load_balancers ||= cf(:resources, [stack_name], type: 'AWS::ElasticLoadBalancingV2::LoadBalancer', quiet: true)
+          def application_load_balancers
+            @_application_load_balancers ||= cf(:resources, [stack_name], type: ['AWS::ElasticLoadBalancingV2::LoadBalancer'], quiet: true)
           end
 
-          def alb_arns
-            app_load_balancers.each_with_object({}) do |alb, h|
-              h[alb.logical_resource_id] = alb.physical_resource_id
+          def alb_names
+            @_alb_names ||= application_load_balancers.map do |alb|
+              alb.physical_resource_id.split('/').fetch(-2)
             end
           end
 
           def alb_status
-            alb_arns.each do |id, arn|
-              debug("ALB status for #{stack_name} #{id}")
-              alb(:instances, [arn], long: true)
+            application_load_balancers.each do |alb|
+              debug("ALB status for #{alb.logical_resource_id}")
+              alb(:instances, [alb.physical_resource_id], long: true)
             end
           end
+        end
+
+        desc 'albs', 'show ALBs for stack'
+        def albs
+          debug("ALBs for stack #{stack_name}")
+          alb(:ls, alb_names, long: true)
         end
 
       end
