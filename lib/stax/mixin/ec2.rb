81d29325985b89d471e011bbb0831e05310f12b7
Richard Lister <rlister@gmail.com>
2017-09-24 22:03:40 -0400

    now we cleaned up the namespace, can mixin Aws to mixin modules

--- lib/stax/mixin/ec2.rb
+++ lib/stax/mixin/ec2.rb
@@ -2,21 +2,21 @@ require 'stax/aws/ec2'
 
 module Stax
   module Ec2Tasks
+    include Aws
 
     def self.included(thor)
-
       thor.class_eval do
 
         desc 'ec2-instances', 'EC2 instances'
         def ec2_instances
-          print_table Aws::Ec2.instances(stack_name).map { |i|
+          print_table Ec2.instances(stack_name).map { |i|
             name = i.tags.find { |t| t.key == 'Name' }&.value
             [
               name,
               i.instance_id,
               i.instance_type,
               i.placement.availability_zone,
-              color(i.state.name, Aws::Ec2::COLORS),
+              color(i.state.name, Ec2::COLORS),
               i.private_ip_address,
               i.public_ip_address
             ]
--- lib/stax/mixin/elb.rb
+++ lib/stax/mixin/elb.rb
@@ -2,27 +2,28 @@ require 'stax/aws/elb'
 
 module Stax
   module ElbTasks
+    include Aws
 
     def self.included(thor)
       thor.class_eval do
 
         no_commands do
           def stack_elbs
-            Aws::Cfn.resources_by_type(stack_name, 'AWS::ElasticLoadBalancing::LoadBalancer')
+            Cfn.resources_by_type(stack_name, 'AWS::ElasticLoadBalancing::LoadBalancer')
           end
         end
 
         desc 'elb-dns', 'ELB DNS names'
         def elb_dns
-          puts Aws::Elb.describe(stack_elbs.map(&:physical_resource_id)).map(&:dns_name)
+          puts Elb.describe(stack_elbs.map(&:physical_resource_id)).map(&:dns_name)
         end
 
         desc 'elb-status', 'ELB instance status'
         def elb_status
           stack_elbs.each do |elb|
             debug("ELB status for #{elb.logical_resource_id} #{elb.physical_resource_id}")
-            print_table Aws::Elb.instance_health(elb.physical_resource_id).map { |i|
-              [i.instance_id, color(i.state, Aws::Elb::COLORS), i.reason_code, i.description]
+            print_table Elb.instance_health(elb.physical_resource_id).map { |i|
+              [i.instance_id, color(i.state, Elb::COLORS), i.reason_code, i.description]
             }
           end
         end
