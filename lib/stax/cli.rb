a181cbe2e7b8adcce406713c35e0def8f13c0ce6
Richard Lister <rlister@gmail.com>
2017-09-28 22:46:40 -0400

    ls needs whole stack_prefix

--- lib/stax/cli.rb
+++ lib/stax/cli.rb
@@ -12,7 +12,7 @@ module Stax
     desc 'ls', 'list stacks for this branch'
     def ls
       print_table Cfn.stacks.select { |s|
-        s.stack_name.start_with?(branch_name)
+        s.stack_name.start_with?(stack_prefix)
       }.map { |s|
         [s.stack_name, s.creation_time, color(s.stack_status, Cfn::COLORS), s.template_description]
       }
