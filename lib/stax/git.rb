cbfe9626ca374f788e1cc341819246106c7a75df
Richard Lister <rlister@gmail.com>
2017-10-02 23:49:51 -0400

    convert git tasks to class methods of a Base subclass

--- lib/stax/git.rb
+++ lib/stax/git.rb
@@ -1,43 +1,55 @@
 require 'git_clone_url'
 
 module Stax
-  class Git
+  class Git < Base
 
-    def self.branch
-      @_branch ||= `git symbolic-ref --short HEAD`.chomp
+    no_commands do
+      def self.branch
+        @_branch ||= `git symbolic-ref --short HEAD`.chomp
+      end
+
+      def self.sha
+        @_sha ||= `git rev-parse HEAD`.chomp
+      end
+
+      def self.short_sha
+        @_short_sha ||= self.sha.slice(0,7)
+      end
+
+      def self.origin_url
+        @_origin_url ||= `git config --get remote.origin.url`.chomp
+      end
+
+      ## path like org/repo
+      def self.repo
+        @_repo ||= GitCloneUrl.parse(origin_url)&.path&.sub(/\.git$/, '')
+      end
+
+      def self.origin_sha
+        @_origin_sha ||= `git rev-parse origin/#{branch}`.chomp
+      end
+
+      def self.toplevel
+        @_toplevel ||= `git rev-parse --show-toplevel`.chomp
+      end
+
+      def self.uncommitted_changes?
+        !`git diff --shortstat`.chomp.empty?
+      end
+
+      def self.unpushed_commits?
+        sha != origin_sha
+      end
     end
 
-    def self.sha
-      @_sha ||= `git rev-parse HEAD`.chomp
+    desc 'branch', 'show current git branch'
+    def branch
+      puts Git.branch
     end
 
-    def self.short_sha
-      @_short_sha ||= self.sha.slice(0,7)
-    end
-
-    def self.origin_url
-      @_origin_url ||= `git config --get remote.origin.url`.chomp
-    end
-
-    ## path like org/repo
-    def self.repo
-      @_repo ||= GitCloneUrl.parse(origin_url)&.path&.sub(/\.git$/, '')
-    end
-
-    def self.origin_sha
-      @_origin_sha ||= `git rev-parse origin/#{branch}`.chomp
-    end
-
-    def self.toplevel
-      @_toplevel ||= `git rev-parse --show-toplevel`.chomp
-    end
-
-    def self.uncommitted_changes?
-      !`git diff --shortstat`.chomp.empty?
-    end
-
-    def self.unpushed_commits?
-      sha != origin_sha
+    desc 'sha', 'show current local git sha'
+    def sha
+      puts Git.sha
     end
 
   end
