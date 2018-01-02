33710177e9ea8ffa92f3c89946f894320ebbaa26
Richard Lister <rlister@gmail.com>
2018-01-02 13:08:30 +0000

    fix indentation

--- lib/stax/mixin/ecs.rb
+++ lib/stax/mixin/ecs.rb
@@ -63,84 +63,84 @@ module Stax
         }
       end
 
-     desc 'tasks', 'ECS tasks for stack'
-     method_option :status, aliases: '-s', type: :string, default: 'RUNNING', desc: 'status to list'
-     def tasks
-       print_table Aws::Ecs.tasks(my.ecs_cluster_name, options[:status].upcase).map { |t|
-         [
-           t.task_arn.split('/').last,
-           t.task_definition_arn.split('/').last,
-           t.container_instance_arn&.split('/')&.last || '--',
-           color(t.last_status, COLORS),
-           "(#{t.desired_status})",
-           t.started_by,
-         ]
-       }
-     end
+      desc 'tasks', 'ECS tasks for stack'
+      method_option :status, aliases: '-s', type: :string, default: 'RUNNING', desc: 'status to list'
+      def tasks
+        print_table Aws::Ecs.tasks(my.ecs_cluster_name, options[:status].upcase).map { |t|
+          [
+            t.task_arn.split('/').last,
+            t.task_definition_arn.split('/').last,
+            t.container_instance_arn&.split('/')&.last || '--',
+            color(t.last_status, COLORS),
+            "(#{t.desired_status})",
+            t.started_by,
+          ]
+        }
+      end
 
-     desc 'containers', 'containers for running tasks'
-     method_option :status, aliases: '-s', type: :string, default: 'RUNNING', desc: 'status to list'
-     def containers
-       debug("Containers for cluster #{my.ecs_cluster_name}")
-       print_table Aws::Ecs.tasks(my.ecs_cluster_name, options[:status].upcase).map { |task|
-         task_defn = task.task_definition_arn.split('/').last
-         task.containers.map { |c|
-           [
-             c.container_arn.split('/').last,
-             c.name,
-             color(c.last_status, COLORS),
-             c.network_interfaces.map(&:private_ipv_4_address).join(','),
-             task_defn,
-             c.exit_code,
-             c.reason,
-           ]
-         }
-       }.flatten(1)
-     end
+      desc 'containers', 'containers for running tasks'
+      method_option :status, aliases: '-s', type: :string, default: 'RUNNING', desc: 'status to list'
+      def containers
+        debug("Containers for cluster #{my.ecs_cluster_name}")
+        print_table Aws::Ecs.tasks(my.ecs_cluster_name, options[:status].upcase).map { |task|
+          task_defn = task.task_definition_arn.split('/').last
+          task.containers.map { |c|
+            [
+              c.container_arn.split('/').last,
+              c.name,
+              color(c.last_status, COLORS),
+              c.network_interfaces.map(&:private_ipv_4_address).join(','),
+              task_defn,
+              c.exit_code,
+              c.reason,
+            ]
+          }
+        }.flatten(1)
+      end
 
-     desc 'instances', 'ECS instances'
-     def instances
-       print_table Aws::Ecs.instances(my.ecs_cluster_name).map { |i|
-         [
-           i.container_instance_arn.split('/').last,
-           i.ec2_instance_id,
-           i.agent_connected,
-           color(i.status, COLORS),
-           i.running_tasks_count,
-           "(#{i.pending_tasks_count})",
-           "agent #{i.version_info.agent_version}",
-           i.version_info.docker_version,
-         ]
-       }
-     end
+      desc 'instances', 'ECS instances'
+      def instances
+        print_table Aws::Ecs.instances(my.ecs_cluster_name).map { |i|
+          [
+            i.container_instance_arn.split('/').last,
+            i.ec2_instance_id,
+            i.agent_connected,
+            color(i.status, COLORS),
+            i.running_tasks_count,
+            "(#{i.pending_tasks_count})",
+            "agent #{i.version_info.agent_version}",
+            i.version_info.docker_version,
+          ]
+        }
+      end
 
-     desc 'run_task [ID]', 'run task by id'
-     def run_task(id)
-       Aws::Ecs.run(my.ecs_cluster_name, Aws::Cfn.id(my.stack_name, id)).tap do |tasks|
-         puts tasks.map(&:container_instance_arn)
-       end
-     end
+      desc 'run_task [ID]', 'run task by id'
+      def run_task(id)
+        Aws::Ecs.run(my.ecs_cluster_name, Aws::Cfn.id(my.stack_name, id)).tap do |tasks|
+          puts tasks.map(&:container_instance_arn)
+        end
+      end
 
-     desc 'stop_task [TASK]', 'stop task'
-     def stop_task(task)
-       Aws::Ecs.stop(my.ecs_cluster_name, task).tap do |task|
-         puts task.container_instance_arn
-       end
-     end
+      desc 'stop_task [TASK]', 'stop task'
+      def stop_task(task)
+        Aws::Ecs.stop(my.ecs_cluster_name, task).tap do |task|
+          puts task.container_instance_arn
+        end
+      end
 
-     desc 'scale', 'scale containers for service'
-     method_option :desired, aliases: '-d', type: :numeric, default: nil, desc: 'desired container count'
-     def scale
-       my.ecs_services.each do |s|
-         debug("Scaling service #{s.logical_resource_id}")
+      desc 'scale', 'scale containers for service'
+      method_option :desired, aliases: '-d', type: :numeric, default: nil, desc: 'desired container count'
+      def scale
+        my.ecs_services.each do |s|
+          debug("Scaling service #{s.logical_resource_id}")
           Aws::Ecs.client.update_service(
             service: s.physical_resource_id,
             desired_count: options[:desired],
           ).service.tap do |s|
             puts "desired: #{s.desired_count}"
           end
-       end
-     end
+        end
+      end
 
     end
 
