4dedc23c90e4af90aeaf625eb0f5e654528b556b
Richard Lister <rlister@gmail.com>
2018-01-12 14:15:58 -0500

    in retrospect this does not belong in a mixin

--- lib/stax/mixin/ec2.rb
+++ lib/stax/mixin/ec2.rb
@@ -37,35 +37,6 @@ module Stax
         }
       end
 
-      desc 'images', 'list AMI images'
-      method_option :owners,   aliases: '-o', type: :array,   default: %w[self], desc: 'self, amazon, aws-marketplace, microsoft'
-      method_option :name,     aliases: '-N', type: :array,   default: nil,      desc: 'names of AMIs to list'
-      method_option :image_id, aliases: '-i', type: :array,   default: nil,      desc: 'image IDs to list'
-      method_option :tag,      aliases: '-t', type: :array,   default: nil,      desc: 'tags as key=value'
-      method_option :tag_key,  aliases: '-T', type: :array,   default: nil,      desc: 'tag keys that should exist'
-      method_option :state,    aliases: '-s', type: :array,   default: nil,      desc: 'available, pending, failed'
-      method_option :number,   aliases: '-n', type: :numeric, default: nil,      desc: 'number of most recent to return'
-      def images
-        filters = [
-          {name: :name,      values: options[:name]},
-          {name: :state,     values: options[:state]},
-          {name: 'image-id', values: options[:image_id]},
-          {name: 'tag-key',  values: options[:tag_key]},
-          options[:tag]&.map do |tag|
-            k, v = tag.split('=')
-            {name: "tag:#{k}", values: [v]}
-          end
-        ].flatten.reject{ |f| f&.fetch(:values).nil? }
-
-        images = Aws::Ec2.images(owners: options[:owners], filters: filters.empty? ? nil : filters)
-        images = images.last(options[:number]) if options[:number]
-
-        print_table images.map { |i|
-          tags = i.tags.map{ |t| "#{t.key}=#{t.value}" }.sort.join(',')
-          [ i.name, i.image_id, i.root_device_type, color(i.state, COLORS), i.creation_date, tags ]
-        }
-      end
     end
-
   end
 end
