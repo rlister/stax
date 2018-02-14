4d6011be8c8627e74d82f73bd93cb271b76397da
Richard Lister <rlister@gmail.com>
2018-02-13 22:21:06 -0500

    safer namespacing

--- lib/stax/cli.rb
+++ lib/stax/cli.rb
@@ -2,8 +2,6 @@ require 'stax/aws/cfn'
 
 module Stax
   class Cli < Base
-    include Aws
-
     class_option :branch, type: :string, default: Git.branch, desc: 'git branch to use'
     class_option :app,    type: :string, default: File.basename(Git.toplevel), desc: 'application name'
 
@@ -14,11 +12,11 @@ module Stax
 
     desc 'ls', 'list stacks for this branch'
     def ls
-      print_table Cfn.stacks.select { |s|
+      print_table Aws::Cfn.stacks.select { |s|
         s.stack_name.start_with?(stack_prefix)
       }.map { |s|
-        [s.stack_name, s.creation_time, color(s.stack_status, Cfn::COLORS), s.template_description]
       }
+        [s.stack_name, s.creation_time, color(s.stack_status, Aws::Cfn::COLORS), s.template_description]
     end
 
   end
