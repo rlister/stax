be3909fea0b7f636cb494fa665f11217b468eb37
Richard Lister <rlister@gmail.com>
2018-02-13 22:22:33 -0500

    sort ls output

--- lib/stax/cli.rb
+++ lib/stax/cli.rb
@@ -15,8 +15,8 @@ module Stax
       print_table Aws::Cfn.stacks.select { |s|
         s.stack_name.start_with?(stack_prefix)
       }.map { |s|
-      }
         [s.stack_name, s.creation_time, color(s.stack_status, Aws::Cfn::COLORS), s.template_description]
+      }.sort
     end
 
   end
