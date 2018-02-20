101ac10cbf934dc9b306244419995ddd79816633
Richard Lister <rlister@gmail.com>
2018-02-20 14:46:59 -0500

    add ECR repo support

--- lib/stax/aws/ecr.rb
+++ lib/stax/aws/ecr.rb
@@ -14,6 +14,12 @@ module Stax
           client.get_authorization_token.authorization_data
         end
 
+        def repositories(opt = {})
+          paginate(:repositories) do |next_token|
+            client.describe_repositories(opt.merge(next_token: next_token))
+          end
+        end
+
         def exists?(repo, tag)
           !client.batch_get_image(repository_name: repo, image_ids: [{image_tag: tag}]).images.empty?
         end
