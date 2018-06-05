fe1d2560548ae81a0cb62da783414038f9430522
Richard Lister <rlister@gmail.com>
2018-06-05 01:33:15 -0400

    add listing of generators

--- lib/stax/generate.rb
+++ lib/stax/generate.rb
@@ -4,8 +4,14 @@ module Stax
   class Cli < Base
 
     desc 'generate NAME [ARGS]', 'run code generators'
-    def generate(generator, *args)
-      Stax::Generators.invoke(generator, *args)
+    def generate(name = nil, *args)
+      if name.nil?
+        Stax::Generators::Base.subclasses.each do |g|
+          say_status(g.command_name, g.desc, :bold)
+        end
+      else
+        Stax::Generators.invoke(name, *args)
+      end
     rescue NameError => e
       fail_task(e.message)
     end
