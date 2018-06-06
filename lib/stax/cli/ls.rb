fd79d8cba48cd3217665de2eb39ccdc4ac33c952
Richard Lister <rlister@gmail.com>
2018-06-06 09:18:16 -0400

    add prefix to ls -a

--- lib/stax/cli/ls.rb
+++ lib/stax/cli/ls.rb
@@ -13,9 +13,9 @@ module Stax
         }.compact
       end
 
-      def ls_stacks_with_prefix
+      def ls_stacks_with_prefix(prefix)
         print_table Aws::Cfn.stacks.select { |s|
-          s.stack_name.start_with?(stack_prefix)
+          s.stack_name.start_with?(prefix || stack_prefix)
         }.map { |s|
           [s.stack_name, s.creation_time, color(s.stack_status, Aws::Cfn::COLORS), s.template_description]
         }.sort
@@ -28,15 +28,15 @@ module Stax
       end
     end
 
-    desc 'ls', 'list stacks'
+    desc 'ls [PREFIX]', 'list stacks'
     method_option :existing, aliases: '-e', type: :boolean, default: false, desc: 'list just existing stacks'
     method_option :all,      aliases: '-a', type: :boolean, default: false, desc: 'list all running stacks with our prefix'
     method_option :account,  aliases: '-A', type: :boolean, default: false, desc: 'list all running stacks in account'
-    def ls
+    def ls(prefix = nil)
       if options[:account]
         ls_account_stacks
       elsif options[:all]
-        ls_stacks_with_prefix
+        ls_stacks_with_prefix(prefix)
       else
         ls_staxfile_stacks
       end
