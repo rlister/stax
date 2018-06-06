d2de5c39b0544dfe9f6949dbcfd263fff023c158
Richard Lister <rlister@gmail.com>
2018-06-06 09:01:45 -0400

    add -a and -A options to ls task

--- lib/stax/cli/ls.rb
+++ lib/stax/cli/ls.rb
@@ -1,13 +1,45 @@
 module Stax
   class Cli < Base
 
-    desc 'ls', 'list stacks for this branch'
+    no_commands do
+      def ls_staxfile_stacks
+        print_table Stax.stack_list.map { |id|
+          name = stack(id).stack_name
+          if (s = Aws::Cfn.describe(name))
+            [s.stack_name, s.creation_time, color(s.stack_status, Aws::Cfn::COLORS), s.description]
+          else
+            options[:existing] ? nil : [name, '-']
+          end
+        }.compact
+      end
+
+      def ls_stacks_with_prefix
+        print_table Aws::Cfn.stacks.select { |s|
+          s.stack_name.start_with?(stack_prefix)
+        }.map { |s|
+          [s.stack_name, s.creation_time, color(s.stack_status, Aws::Cfn::COLORS), s.template_description]
+        }.sort
+      end
+
+      def ls_account_stacks
+        print_table Aws::Cfn.stacks.map { |s|
+          [s.stack_name, s.creation_time, color(s.stack_status, Aws::Cfn::COLORS), s.template_description]
+        }.sort
+      end
+    end
+
+    desc 'ls', 'list stacks'
+    method_option :existing, aliases: '-e', type: :boolean, default: false, desc: 'list just existing stacks'
+    method_option :all,      aliases: '-a', type: :boolean, default: false, desc: 'list all running stacks with our prefix'
+    method_option :account,  aliases: '-A', type: :boolean, default: false, desc: 'list all running stacks in account'
     def ls
-      print_table Aws::Cfn.stacks.select { |s|
-        s.stack_name.start_with?(stack_prefix)
-      }.map { |s|
-        [s.stack_name, s.creation_time, color(s.stack_status, Aws::Cfn::COLORS), s.template_description]
-      }.sort
+      if options[:account]
+        ls_account_stacks
+      elsif options[:all]
+        ls_stacks_with_prefix
+      else
+        ls_staxfile_stacks
+      end
     end
 
   end
