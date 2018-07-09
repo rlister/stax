a473cd18b530708a303a375299a975de346abcf4
Richard Lister <rlister@gmail.com>
2018-07-09 17:06:22 -0400

    add endpoints task

--- lib/stax/mixin/rds.rb
+++ lib/stax/mixin/rds.rb
@@ -20,16 +20,29 @@ module Stax
         def stack_db_instances
           Aws::Cfn.resources_by_type(my.stack_name, 'AWS::RDS::DBInstance')
         end
+
+        def stack_rds_instances
+          filter = { name: 'db-instance-id', values: stack_db_instances.map(&:physical_resource_id) }
+          Aws::Rds.instances(filters: [filter])
+        end
       end
 
       desc 'instances', 'list db instances for stack'
       def instances
         debug("RDS DB instances for #{my.stack_name}")
-        filter = { name: 'db-instance-id', values: stack_db_instances.map(&:physical_resource_id) }
-        print_table Aws::Rds.instances(filters: [filter]).map { |i|
+        print_table stack_rds_instances.map { |i|
           [i.db_instance_identifier, i.engine, i.engine_version, color(i.db_instance_status, COLORS), i.db_instance_class, i.db_subnet_group&.vpc_id, "ha:#{i.multi_az.to_s}"]
         }
       end
+
+      desc 'endpoints', 'list db instance endpoints'
+      def endpoints
+        debug("RDS DB endpoints for #{my.stack_name}")
+        print_table stack_rds_instances.map { |i|
+          [i.db_instance_identifier, i.endpoint&.address, i.endpoint&.port, i.endpoint&.hosted_zone_id]
+        }
+      end
+
 
     end
   end
