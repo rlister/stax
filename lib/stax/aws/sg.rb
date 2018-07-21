62f436848e2af4a2ef371ec654da828b5a04ff19
Richard Lister <rlister@gmail.com>
2018-07-21 15:49:54 -0400

    add methods to auth and revoke SGs from other SGs

--- lib/stax/aws/sg.rb
+++ lib/stax/aws/sg.rb
@@ -24,6 +24,22 @@ module Stax
           warn(e.message)
         end
 
+        def authorize_sg(id, sg, port)
+          client.authorize_security_group_ingress(
+            group_id: id,
+            ip_permissions: [
+              {
+                ip_protocol: :tcp,
+                from_port: port,
+                to_port: port,
+                user_id_group_pairs: [ { group_id: sg } ],
+              }
+            ]
+          )
+        rescue ::Aws::EC2::Errors::InvalidPermissionDuplicate => e
+          warn(e.message)
+        end
+
         def revoke(id, cidr, port = 22)
           client.revoke_security_group_ingress(
             group_id:    id,
@@ -36,6 +52,21 @@ module Stax
           warn(e.message)
         end
 
+        def revoke_sg(id, sg, port)
+          client.revoke_security_group_ingress(
+            group_id: id,
+            ip_permissions: [
+              {
+                ip_protocol: :tcp,
+                from_port: port,
+                to_port: port,
+                user_id_group_pairs: [ { group_id: sg } ],
+              }
+            ]
+          )
+        rescue ::Aws::EC2::Errors::InvalidPermissionNotFound => e
+          warn(e.message)
+        end
       end
     end
   end
