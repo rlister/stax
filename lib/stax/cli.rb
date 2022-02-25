d4c8b63be17cfed4a9da9876c92de0b9b6b0424f
Richard Lister <rlister@gmail.com>
2022-02-25 16:27:31 -0500

    whitespace

--- lib/stax/cli.rb
+++ lib/stax/cli.rb
@@ -9,6 +9,7 @@ module Stax
   class Cli < Base
     class_option :branch, type: :string, default: Git.branch, desc: 'git branch to use'
     class_option :app,    type: :string, default: File.basename(Git.toplevel), desc: 'application name'
+
     ## silence deprecation warning
     ## https://github.com/erikhuda/thor/blob/fb625b223465692a9d8a88cc2a483e126f1a8978/CHANGELOG.md#100
     def self.exit_on_failure?
